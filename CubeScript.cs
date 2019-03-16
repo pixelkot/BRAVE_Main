d6ed2c6f8e5824b709d69dc0c76c1ee578721afe
databears-cal <45097648+databears-cal@users.noreply.github.com>
2019-03-16 15:18:43 -0700

    Controller Script/Documentation Completed

---
+++ CubeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public bool activated;
+    public Vector3 up, down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+        up = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!activated)
+        {
+            transform.position = Vector3.Lerp(transform.position, up, Time.deltaTime * 5f);
+        }
+        if (activated)
+        {
+            transform.position = Vector3.Lerp(transform.position, down, Time.deltaTime * 5f);
+        }
+    }
+
+    void OnVRTriggerDown()
+    {
+        activated = true;
+    }
+}
